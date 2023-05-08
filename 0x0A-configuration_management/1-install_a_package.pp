bc231ce4ec2d8023913d452d3c5c58201b8d85bd
Christian <c.sangwa@yahoo.fr>
2023-05-08 05:18:47 -0700

    fixed script

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,7 @@
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install-flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path => '/usr/bin',
-  require => Package['python3-pip'],
+#!/usr/bin/pup
+# Install flask(2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
 
