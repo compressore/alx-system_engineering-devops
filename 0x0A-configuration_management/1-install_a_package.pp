cfecca1588e6852592fc210d3e587e1b60e21f8c
Christian <c.sangwa@yahoo.fr>
2023-05-08 02:12:10 -0700

    Using Puppet, install flask from pip3

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,10 @@
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install-flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path => '/usr/bin',
+  require => Package['python3-pip'],
+}
+
